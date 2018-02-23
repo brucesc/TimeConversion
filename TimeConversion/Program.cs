4600b67f13f001e4b5b16c640e857cd7260a2da7
Steven Bruce <Brucesc726@gmail.com>
2018-02-23 11:22:46 -0500

    initial

---
+++ TimeConversion/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+class Solution
+{
+
+    static string timeConversion(string s)
+    {
+        DateTime timeValue = Convert.ToDateTime(s);
+        string military = timeValue.ToString("HH:mm:ss");
+        return military;
+    }
+
+    static void Main(String[] args)
+    {
+        string s = Console.ReadLine();
+        string result = timeConversion(s);
+        Console.WriteLine(result);
+        Console.ReadKey();
+
+    }
+}
